ff9009c1b170bc578252630b199373d57fda048c
gmavssh <gmavsks@gmail.com>
2022-07-06 22:10:42 +0900

    algorithm
    daily study

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,10 +1,11 @@
 import Algorithm.TestCase;
 import Algorithm.programmers.level1.Average;
+import Algorithm.programmers.level1.Remainder;
 
 
 public class Algorithm {
     public static void main(String[] args) {
-        TestCase testCase = new Average();
+        TestCase testCase = new Remainder();
         testCase.test();
     }
 }
