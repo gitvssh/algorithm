7b87f682e6d8e6ea7aa4f8f82dc73f9fe881424d
gmavssh <gmavsks@gmail.com>
2022-07-10 23:23:01 +0900

    algorithm
    daily study

--- src/Algorithm/programmers/Test.java
+++ src/Algorithm/programmers/Test.java
@@ -2,12 +2,11 @@ package Algorithm.programmers;
 
 import Algorithm.TestCase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test implements TestCase {
     @Override
     public void test() {
-        long n = 1;
-        boolean b = (Math.sqrt(n) - (int) Math.sqrt(n)) == 0 ? true : false;
-
-        System.out.println("b = " + b);
     }
 }
