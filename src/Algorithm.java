078a37c87d94461465563071129a2d8ec86e6348
gmavssh <gmavsks@gmail.com>
2022-07-13 22:48:42 +0900

    algorithm
    daily study

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,10 +1,11 @@
 import Algorithm.TestCase;
 import Algorithm.programmers.level1.SecretMap;
+import Algorithm.programmers.level1.StrangeWord;
 
 
 public class Algorithm {
     public static void main(String[] args) {
-        TestCase testCase = new SecretMap();
+        TestCase testCase = new StrangeWord();
         testCase.test();
     }
 }
