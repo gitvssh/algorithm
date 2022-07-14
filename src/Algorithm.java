4a976982fa842bddbb27eed5b7668b6a7b69d56a
gmavssh <gmavsks@gmail.com>
2022-07-14 23:44:18 +0900

    algorithm
    daily study

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,11 +1,13 @@
 import Algorithm.TestCase;
+import Algorithm.programmers.level1.CaesarCipher;
+import Algorithm.programmers.level1.CollatzConjecture;
 import Algorithm.programmers.level1.SecretMap;
 import Algorithm.programmers.level1.StrangeWord;
 
 
 public class Algorithm {
     public static void main(String[] args) {
-        TestCase testCase = new StrangeWord();
+        TestCase testCase = new CaesarCipher();
         testCase.test();
     }
 }
