dd0b3633ca1a809849e736e660eab580aa15e142
gitvssh <gmavsks@gmail.com>
2022-08-30 18:23:28 +0900

    daily study
    algorithm

--- src/algorithm/leetcode/mlp/LeetAddTwoNumbers.java
+++ src/algorithm/leetcode/mlp/LeetAddTwoNumbers.java
@@ -5,15 +5,25 @@ import algorithm.TestCase;
 
 public class LeetAddTwoNumbers implements TestCase {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        result.val=(l1.val+l2.val)%10;
-        int red = (l1.val+l2.val)/10;
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
 
-        while(l1.next!=null||l2.next!=null){
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+
 
         }
 
-        return result;
+        return head.next;
     }
 
     @Override
