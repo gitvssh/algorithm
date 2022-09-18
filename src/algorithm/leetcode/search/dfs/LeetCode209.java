212af3499180cc4d2920da023eaa90808e2c88c9
DESKTOP-6858CPU\gmavs <gmavsks@gmail.com>
2022-09-18 21:06:38 +0900

    daily study
    algorithm

--- src/algorithm/leetcode/search/dfs/LeetCode209.java
+++ src/algorithm/leetcode/search/dfs/LeetCode209.java
@@ -1,7 +1,5 @@
 package algorithm.leetcode.search.dfs;
 
-import java.util.Scanner;
-
 /*
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
 You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
@@ -9,6 +7,8 @@ For example, the pair [0, 1], indicates that to take course 0 you have to first 
 Return true if you can finish all courses. Otherwise, return false.
 */
 public class LeetCode209 {
+
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(prerequisites.length==1) return true;
         if(prerequisites.length==0) return false;
@@ -18,8 +18,7 @@ public class LeetCode209 {
         int[] cache = new int[m];
         String a = "a";
 //        Arrays.stream(prerequisites).filter(y -> )
-
-
+        
         for (int i = 0; i < numCourses; i++) {
             if (!checkPrq(prerequisites, i, m, cache)) {
                 result = false;
@@ -34,8 +33,8 @@ public class LeetCode209 {
         if (cache[i] != 0) return true;
         if(i >= m) return false;
         int nextCourse = prerequisites[i][1];
-        if (i < 0 || i == prerequisites[nextCourse][1]) return false;
-        boolean result = false;
+        if (i == prerequisites[nextCourse][1]) return false;
+        boolean result;
         result = checkPrq(prerequisites, prerequisites[i][1],m, cache);
         cache[i] = 1;
         return result;
