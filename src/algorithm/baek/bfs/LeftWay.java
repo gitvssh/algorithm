0eda21480dd3bddd8ab7455826a2edeedb3f62bf
gmavsks <gmavsks@gmail.com>
2022-10-14 22:22:13 +0900

    algorithm study

--- src/algorithm/baek/bfs/LeftWay.java
+++ src/algorithm/baek/bfs/LeftWay.java
@@ -5,9 +5,6 @@ import algorithm.TestCase;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class LeftWay implements TestCase {
     static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
@@ -15,12 +12,12 @@ public class LeftWay implements TestCase {
     //WWWWWWBWBBBWWWWBBWW
     //BBBWWWWWWWWWWBWWWWB
     /*
-    * 1. calculate longest way length (DFS or BFS)
-    * 2. calculate shortest way length (BFS)
-    * 3. minus 1. - 2.
-    *
-    * * can go up,right,down
-    * */
+     * 1. calculate longest way length (DFS or BFS)
+     * 2. calculate shortest way length (BFS)
+     * 3. minus 1. - 2.
+     *
+     * * can go up,right,down
+     * */
     @Override
     public void test() throws ParseException {
 
@@ -31,29 +28,29 @@ public class LeftWay implements TestCase {
     private static byte[] buffer;
     private static int curIdx, maxIdx;
 
-    private static void initFI(){
+    private static void initFI() {
         inputStream = new DataInputStream(System.in);
         buffer = new byte[DEFAULT_BUFFER_SIZE];
         curIdx = maxIdx = 0;
     }
 
-    private static int nextInt() throws IOException{
+    private static int nextInt() throws IOException {
         int ret = 0;
         byte c = read();
         while (c <= ' ') c = read();
-            boolean neg = (c == '-');
+        boolean neg = (c == '-');
         if (neg) c = read();
-        do{
+        do {
             ret = ret * 10 + c - '0';
         } while ((c = read()) >= '0' && c <= '9');
-        if( neg) return -ret;
+        if (neg) return -ret;
         return ret;
     }
 
-    private static byte read() throws IOException{
-        if (curIdx == maxIdx){
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
             maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
-            if(maxIdx == -1) buffer[0] = -1;
+            if (maxIdx == -1) buffer[0] = -1;
         }
         return buffer[curIdx++];
     }
