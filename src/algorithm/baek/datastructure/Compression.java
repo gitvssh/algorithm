735e15c57cd2079f5dcdf7a6bf897077617e98c9
gmavsks <gmavsks@gmail.com>
2022-10-19 17:59:56 +0900

    algorithm study

--- src/algorithm/baek/datastructure/Compression.java
+++ src/algorithm/baek/datastructure/Compression.java
@@ -4,7 +4,6 @@ import algorithm.TestCase;
 import lombok.extern.slf4j.Slf4j;
 
 import java.text.ParseException;
-import java.util.Iterator;
 import java.util.Stack;
 
 
@@ -14,39 +13,41 @@ public class Compression implements TestCase {
         int len = str.length();
         char[] chars = str.toCharArray();
         int res = 0;
-        int curIdx = 0;
         Stack<Character> st = new Stack<>();
-
-        while (curIdx < len) {
-            char curChar = chars[curIdx];
-            if (curChar == '(') {
-                int count = 0;
-                while (chars[curIdx] != ')') {
-                    count++;
-                    curIdx++;
-                }
-                log.info("{}",count);
-                res += (count * (st.pop() - '0'));
-            } else {
-                st.add(curChar);
-                curIdx++;
-            }
-            log.info("curIdx: {} / {} : {}", curIdx, len, res);
+        for (int i = 0; i < len; i++) {
+            st.add(chars[i]);
         }
-
-        Iterator<Character> it = st.iterator();
-
-        while (it.hasNext()) {
-            log.info("{}",st.pop());
-            res++;
+        while (!st.isEmpty()) {
+            if (st.pop() == ')') {
+                res += countBracket(st);
+            } else {
+                res++;
+            }
         }
         return res;
     }
 
+    private int countBracket(Stack<Character> st) {
+        if (st.isEmpty()) {
+            return 0;
+        }
+        int cnt = 0;
+        while (st.peek() != '(') {
+            if (st.pop() == ')') {
+                cnt += countBracket(st);
+            } else {
+                cnt++;
+            }
+        }
+        st.pop();
+        int multiple = st.isEmpty() ? 0 : st.pop() - '0';
+        log.info("cnt: {}, multiple: {}", cnt, multiple);
+        return cnt * multiple;
+    }
+
     @Override
     public void test() throws ParseException {
-        int compress = compress("33(562(71(9)))");
-        log.info("is start");
+        int compress = compress("1()66(5)");
         log.info("compress = {}", compress);
     }
 }
