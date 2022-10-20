3323ef430abe089889786208520ca9357b6e226b
gmavsks <gmavsks@gmail.com>
2022-10-20 17:05:00 +0900

    algorithm study

--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -1,10 +1,11 @@
 package algorithm;
 
 import algorithm.baek.datastructure.Compression;
+import lombok.extern.slf4j.Slf4j;
 
 import java.text.ParseException;
 
-
+@Slf4j
 public class Algorithm {
     public static void main(String[] args) {
         TestCase testCase = new Compression();
