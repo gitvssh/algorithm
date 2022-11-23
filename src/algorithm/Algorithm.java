1f8b24c8a28f34f379e75228a38bdbbc73a8a043
gmavsks <gmavsks@gmail.com>
2022-11-23 10:34:19 +0900

    daily study
    algorithm

--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -5,13 +5,16 @@ import algorithm.baek.set.NumberCard;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 //@Slf4j
 public class Algorithm {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         NumberCard testCase = new NumberCard();
-
+        ArrayList<String> list = new ArrayList<>();
+        list.add("gef");
+        list.add("dbf");
+        list.stream().sorted().forEach((i) -> list.get(0));
         doTest(testCase);
     }
 
