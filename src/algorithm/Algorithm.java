41e9b7069bec0c3c7b25ffcfb26d8caec1161a71
gitvssh <39320478+gitvssh@users.noreply.github.com>
2022-11-26 12:51:48 +0900

    daily study
    algorithm

--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -1,16 +1,14 @@
 package algorithm;
 
-
-import algorithm.baek.set.SubSetString;
+import algorithm.programmers.codingtest.devmatch2022winter.Marathoner;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 
 //@Slf4j
 public class Algorithm {
     public static void main(String[] args) {
-        SubSetString testCase = new SubSetString();
+        Marathoner testCase = new Marathoner();
         doTest(testCase);
     }
 
