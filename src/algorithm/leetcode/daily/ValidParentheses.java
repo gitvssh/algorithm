b3128813613c729df0a248257d5984f4ad8dd47c
gitvssh <39320478+gitvssh@users.noreply.github.com>
2022-11-28 21:25:41 +0900

    daily study
    algorithm

--- src/algorithm/leetcode/daily/ValidParentheses.java
+++ src/algorithm/leetcode/daily/ValidParentheses.java
@@ -12,6 +12,7 @@ public class ValidParentheses implements TestCase {
     public void test() throws ParseException, IOException {
         String s = "]";
         System.out.println(isValid(s));
+
     }
 
     public boolean isValid(String s) {
