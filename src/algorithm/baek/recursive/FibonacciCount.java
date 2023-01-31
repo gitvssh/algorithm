1c5dce2d92bbd00583373aabe499a55fe5e4a415
lsh <gmavsks@imbsystem.com>
2023-01-31 09:01:29 +0900

    daily study
    algorithm

--- src/algorithm/baek/recursive/FibonacciCount.java
+++ src/algorithm/baek/recursive/FibonacciCount.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 public class FibonacciCount implements TestCase {
 
     public static Integer[][] dp = new Integer[41][2];
+    public static int[] dp2 = new int[41];
 
     @Override
     public void test() throws ParseException, IOException {
@@ -17,12 +18,15 @@ public class FibonacciCount implements TestCase {
         dp[0][1] = 0;
         dp[1][0] = 0;
         dp[1][1] = 1;
+        dp2[0] = 1;
+        dp2[1] = 1;
 
         int tc = Integer.parseInt(br.readLine());
         for (int i = 0; i < tc; i++) {
             int n = Integer.parseInt(br.readLine());
             Integer[] fibonacci = fibonacci(n);
             bw.write(fibonacci[0] + " " + fibonacci[1] + "\n");
+            bw.write(fibonacci2(n) + "\n");
         }
         bw.close();
     }
@@ -33,5 +37,12 @@ public class FibonacciCount implements TestCase {
             dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
         }
         return dp[n];
+    }
+
+    public int fibonacci2(int n) {
+        if (dp2[n] == 0) {
+            dp2[n] = fibonacci2(n - 1) + fibonacci2(n - 2);
+        }
+        return dp2[n];
     }
 }
