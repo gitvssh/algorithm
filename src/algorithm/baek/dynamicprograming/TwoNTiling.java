d55710eb39b827e40156d38fd2adaec456a61ce1
lsh <gmavsks@imbsystem.com>
2023-02-07 16:56:09 +0900

    daily study
    algorithm

--- src/algorithm/baek/dynamicprograming/TwoNTiling.java
+++ src/algorithm/baek/dynamicprograming/TwoNTiling.java
@@ -16,6 +16,10 @@ public class TwoNTiling implements TestCase {
     public void test() throws ParseException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
