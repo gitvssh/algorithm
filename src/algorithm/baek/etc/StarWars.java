0aac71d868c12e83700895631ca8069e36a6783e
gitvssh <39320478+gitvssh@users.noreply.github.com>
2023-03-18 23:55:20 +0900

    daily algorithm
    백준 출력 문제

---
+++ src/algorithm/baek/etc/StarWars.java
@@ -0,0 +1,24 @@
+package algorithm.baek.etc;
+
+import algorithm.TestCase;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class StarWars implements TestCase {
+    @Override
+    public void test() throws ParseException, IOException {
+        System.out.println("    8888888888  888    88888\n" +
+                "   88     88   88 88   88  88\n" +
+                "    8888  88  88   88  88888\n" +
+                "       88 88 888888888 88   88\n" +
+                "88888888  88 88     88 88    888888\n" +
+                "\n" +
+                "88  88  88   888    88888    888888\n" +
+                "88  88  88  88 88   88  88  88\n" +
+                "88 8888 88 88   88  88888    8888\n" +
+                " 888  888 888888888 88  88      88\n" +
+                "  88  88  88     88 88   88888888");
+    }
+}
+
